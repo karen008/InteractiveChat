dce7d8626d75bd438c3a6d26d835abfcccb95bc9
Karen Hakobyan <karenhakobyan2001@gmail.com>
2019-12-21 13:00:36 +0400

    Second version

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,67 @@ import java.util.Scanner;
 public class Main {
 
     public static String chat_history = "";
+    public static int num;
+    public static String[] members;
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Select number of chat members: ");
-        int num = sc.nextInt();
-        for (int i = 1; i <num+1 ; i++) {
+        num = sc.nextInt();
+        members = new String[num];
 
-            System.out.println("Member "+i);
-            System.out.println("Press number for action");
-            System.out.println("1. Send message");
-            System.out.println("2. Exit chat");
+        for (int j = 0; j < num; j++) {
+            members[j] = "Member " + (j + 1);
+        }
 
-            Scanner sc_select = new Scanner(System.in);
+        while (members.length>1) {
+            for (int i = 0; i < members.length; i++) {
+                System.out.println(members[i]);
+                System.out.println("Press number for action");
+                System.out.println("1. Send message");
+                System.out.println("2. Exit chat");
 
-            int num_select = sc_select.nextInt();
+                Scanner sc_select = new Scanner(System.in);
+                int num_select = sc_select.nextInt();
 
-            switch (num_select){
-                case 1 :
-                    Scanner chat_message = new Scanner(System.in);
-                    System.out.println("Enter your message");
-                    DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-                    Date date = new Date();
-                    String message = dateFormat.format(date) + " : " + "Member " + i + " : " + chat_message.nextLine();
-                    chat_history += message+ "\n";
-                case 2 :
-                    System.out.println("You have exited chat");
-
+                switch (num_select) {
+                    case 1:
+                        Scanner chat_message = new Scanner(System.in);
+                        System.out.println("Enter your message");
+                        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                        Date date = new Date();
+                        String message = dateFormat.format(date) + " : " + members[i] + " : " + chat_message.nextLine();
+                        chat_history += message + "\n";
+                        break;
+                    case 2:
+                        System.out.println("You have exited chat");
+                        System.out.println(" ");
+                        members = removeTheElement(members, i);
+                        break;
+                }
             }
         }
 
         System.out.println(chat_history);
 
+    }
+
+    public static String[] removeTheElement(String[] arr, int index) {
+        if (arr == null || index < 0 || index >= arr.length) {
+            return arr;
+        }
+
+        String[] anotherArray = new String[arr.length - 1];
+
+        for (int i = 0, k = 0; i < arr.length; i++) {
+
+            if (i == index) {
+                continue;
+            }
+
+            anotherArray[k++] = arr[i];
+        }
+        return anotherArray;
     }
 }
